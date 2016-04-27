018cd3a1e9985b4df31d86f2969c5b8a9038b8ba
jerridan <jerridan@gmail.com>
2016-04-26 20:48:29 -0400

    first commit; initial express, skeleton setup

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var lessMiddleware = require('less-middleware');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+app.use(lessMiddleware(__dirname + '/styles'));
+app.use(express.static(__dirname + '/styles'));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+var server = app.listen(3000, function() {
+  console.log('server running at http://localhost:' + server.address().port);
+});
