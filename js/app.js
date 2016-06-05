c2a32d49da9ddd9f6fe422d91771cc05075b12bb
jerridan <jerridan@gmail.com>
2016-06-05 11:33:36 -0400

    added nav highlighting; added jquery; finished desktop and tablet version of home section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-$(document).foundation();
+// open/close mobile menu
+if ($(window).width() < 640) {
+  $('#menu-icon').bind('touchstart', function () {
+    $('#main-menu').toggleClass('open');
+  });
+}
+
+// highlight menu based on page navigation
+$(document).scroll(function () {
+  $('nav ul li a[href="#' + $('section:hover').attr('id') + '"]').addClass('active').siblings('nav a').removeClass('active');
+});
