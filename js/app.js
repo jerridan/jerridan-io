effd0db5640363f9988c246120cee454388dfb09
jerridan <jerridan@gmail.com>
2016-06-06 01:46:31 -0400

    Finished design for project info modals; completed first project description

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // open/close mobile menu
 if ($(window).width() < 640) {
-  $('#menu-icon').bind('touchstart', function () {
+  $('#menu-icon').on('touchend', function () {
     $('#main-menu').toggleClass('open');
   });
 }
@@ -8,4 +8,20 @@ if ($(window).width() < 640) {
 // highlight menu based on page navigation
 $(document).scroll(function () {
   $('nav ul li a[href="#' + $('section:hover').attr('id') + '"]').addClass('active').siblings('nav ul li a').removeClass('active');
+});
+
+// trigger modals for project details when card are clicked
+$('.md-trigger').each(function () {
+  $(this).on('touchend click', function () {
+    var modal_id = '#' + $(this).attr('data-modal') + '-modal';
+    $(modal_id).addClass('md-show');
+  });
+});
+
+$('.md-close, .md-overlay').each(function () {
+  $(this).on('touchend click', function () {
+    $('.md-modal').each(function() {
+      $(this).removeClass('md-show');
+    });
+  });
 });
