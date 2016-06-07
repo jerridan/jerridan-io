10ff228cb6198b96f5de7290bde49694f133322b
jerridan <jerridan@gmail.com>
2016-06-07 16:31:29 -0400

    Fixed menu highlighting and navigation

--- js/app.js
+++ js/app.js
@@ -8,11 +8,47 @@ if ($(window).width() < 640) {
   $('#menu-icon').on('touchend', function () {
     $('#main-menu').toggleClass('open');
   });
+  $('#main-menu li a').on('touchend', function (evt) {
+    evt.preventDefault();
+    $('#main-menu').toggleClass('open');
+    var anchor = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(anchor).position().top}, 500);
+  });
+} else {
+  $('#main-menu li a').on('touchend click', function (evt) {
+    evt.preventDefault();
+    var anchor = $(this).attr('href');
+    if (anchor === '#skills') {
+      if ($('#skills-contact').height() <= $(window).height()) {
+        $('html, body').animate({scrollTop: $(document).height() - $(window).height() - 20}, 500);
+      } else {
+        $('html, body').animate({scrollTop: $(anchor).position().top}, 500);
+      }
+    } else {
+      $('html, body').animate({scrollTop: $(anchor).position().top}, 500);
+    }
+  });
 }
 
 // highlight menu based on page navigation
+var nav_boundary_amount = $(window).height() / 3;
 $(document).scroll(function () {
-  $('nav ul li a[href="#' + $('section:hover').attr('id') + '"]').addClass('active').siblings('nav ul li a').removeClass('active');
+  if ($(window).scrollTop() === ($(document).height() - $(window).height())) {
+    $('section').each(function () {
+      $('nav ul li a[href="#' + $(this).attr('id') + '"]').removeClass('active');
+    });
+    $('nav ul li a[href="#contact"]').addClass('active');
+  } else {
+    $('section').each(function () {
+      if ($(window).scrollTop() >= ($(this).position().top - nav_boundary_amount) &&
+        $(window).scrollTop() < ($(this).position().top + $(this).height() - nav_boundary_amount)) {
+        $('nav ul li a[href="#' + $(this).attr('id') + '"]').addClass('active');
+      } else {
+        $('nav ul li a[href="#' + $(this).attr('id') + '"]').removeClass('active');
+      }
+    });
+  }
+
 });
 
 // trigger modals for project details when card are clicked
